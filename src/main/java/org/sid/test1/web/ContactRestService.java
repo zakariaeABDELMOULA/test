24ddd46d170a538e49526d9e0957084e74c4e134
ho <ho@192.168.1.16>
2019-04-11 10:05:43 +0200

    fichier de crud back end exemple contact

---
+++ src/main/java/org/sid/test1/web/ContactRestService.java
@@ -0,0 +1,59 @@
+package org.sid.test1.web;
+import java.util.List;
+
+import org.sid.dao.ContactRepository;
+import org.sid.entities.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin("*")
+public class ContactRestService {
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@RequestMapping(value = "/contacts",method = RequestMethod.GET)
+	private List<Contact> getContacts(){
+		return contactRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/contacts/{id}",method = RequestMethod.GET)
+	private Contact getContacts(@PathVariable Long id){
+		return contactRepository.findOne(id);
+	}
+	
+	@RequestMapping(value = "/contacts",method = RequestMethod.POST)
+	private Contact save(@RequestBody Contact contact){
+		return contactRepository.save(contact);
+	}
+	
+	@RequestMapping(value = "/contacts/{id}",method = RequestMethod.PUT)
+	private Contact update(@PathVariable Long id, @RequestBody Contact contact){
+		contact.setId(id);
+		return contactRepository.save(contact);
+	}
+	
+	@RequestMapping(value = "/contacts/{id}",method = RequestMethod.DELETE)
+	private boolean supprimer(@PathVariable Long id){
+		contactRepository.delete(id);
+		return true;
+	}
+	
+	@RequestMapping(value = "/chercherContacts",method = RequestMethod.GET)
+	private Page<Contact> chercher(
+			@RequestParam(name="mc",defaultValue="")String mc,
+			@RequestParam(name="page",defaultValue="0")int  page,
+			@RequestParam(name="size",defaultValue="5")int  size)
+	{
+		return contactRepository.chercher("%"+mc+"%",new PageRequest(page, size));
+	}
+}
