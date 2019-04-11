c83ed2491d5fbeb8423f76e9c5360eaae180f1dc
ABDELMOULA <ABDELMOULA@DESKTOP-ILMHJGB>
2019-04-11 10:32:25 +0100

    add Hotel Lodel

--- src/main/java/org/sid/entities/Hotel.java
+++ src/main/java/org/sid/entities/Hotel.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
+// Entity Hotel
 @Entity
 public class Hotel implements Serializable{
 	@Id @GeneratedValue
